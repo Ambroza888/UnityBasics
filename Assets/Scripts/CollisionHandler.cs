47015356492b9f6bbe009ff8974f387016c16152
Veselin Mladenov <Ambroza8888@gmail.com>
2021-05-27 10:24:18 -0700

    added collision script

--- Assets/Scripts/CollisionHandler.cs
+++ Assets/Scripts/CollisionHandler.cs
@@ -1,7 +1,9 @@
-﻿using UnityEngine;
-
+﻿using System;
+using UnityEngine;
+using UnityEngine.SceneManagement;
 public class CollisionHandler : MonoBehaviour
 {
+    [SerializeField] float levelLoadDelay = 2f;
      private void OnCollisionEnter(Collision other) 
      {
           switch (other.gameObject.tag)
@@ -10,14 +12,44 @@ public class CollisionHandler : MonoBehaviour
                 Debug.Log("This thing is Friendly");
                 break;
             case "Finish":
-                Debug.Log("Congrats, yo, you finished");
+                StartSuccessSequence();
                 break;
             case "Fuel":
                 Debug.Log("yOU PICKED UP FUEL");
                 break;
             default:
-                Debug.Log("Sorry, you blew up");
+                StartCrashSequense();
                 break;
           } 
+     }
+
+    private void StartSuccessSequence()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("LoadNextLevel", levelLoadDelay);
+    }
+
+    void StartCrashSequense()
+    {
+        GetComponent<Movement>().enabled = false;
+        Invoke("ReloadLevel", levelLoadDelay);
+    }
+
+    void LoadNextLevel()
+    {
+        int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+        int nextSceneIndex = currentSceneIndex + 1;
+
+        if (nextSceneIndex == SceneManager.sceneCountInBuildSettings)
+        {
+            nextSceneIndex = 0;
+        }
+        SceneManager.LoadScene(nextSceneIndex);
+    }
+
+    void ReloadLevel()
+     {
+         int currentSceneIndex = SceneManager.GetActiveScene().buildIndex;
+         SceneManager.LoadScene(currentSceneIndex);
      }
 }
